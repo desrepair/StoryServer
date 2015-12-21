be8eb75458d37539193931350f96133bb3941e24
Aaron Zakem <Aaron Zakem>
2015-12-20 20:11:09 -0500

    Added NYT/Guardian and News Tweet calls

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var conn = mysql.createConnection({ // stories DB
     port     : '3306',
     user     : 'topstory',
     password : 'topstory',
-    database : 'stories',
+    database : 'stories'
 });
 
 var connTweets = mysql.createConnection({ // general tweets DB
@@ -22,11 +22,16 @@ var connTweets = mysql.createConnection({ // general tweets DB
   port     : '3306',
   user     : 'venciya',
   password : 'venciyam',
-  database : 'TweetMap',
+  database : 'TweetMap'
 });
 
-var newsSubjects = {};
-newsSubjects.subjects = [];
+var connNewsTweets = mysql.createConnection ({ // news tweets DB
+    host     : '',
+    port     : '',
+    user     : 'venciya',
+    password : 'venciyam',
+    database : ''
+});
 
 app.listen(3000);
 console.log('Express started on port 3000.');
@@ -36,6 +41,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// return top 5 news subjects from all sources
 app.get('/newsSubjects', function(req, res) {
     conn.query('SELECT * FROM TopSubjects', function(err, rows, fields) {
         var subjects = [];
@@ -52,6 +58,7 @@ app.get('/newsSubjects', function(req, res) {
     });
 });
 
+// return stories from all sources matching subject provided in request body
 app.post('/topStories', function(req, res) {
     var sqlResults = 'SELECT * FROM TopStories WHERE Subjects LIKE \'%' + req.body.subject  + '%\';';
     conn.query(sqlResults, function(err, rows, fields) {
@@ -69,6 +76,42 @@ app.post('/topStories', function(req, res) {
     });
 });
 
+// return top 5 news subjects from NYT and Guardian
+app.get('/seriousNewsSubjects', function(req, res) {
+    conn.query('SELECT * FROM TopSeriousSubjects', function(err, rows, fields) {
+        var subjects = [];
+        if (err) {
+            console.log(err);
+        } else {
+            for (var i in rows) {
+                subjects.push(rows[i].Subject);
+                if (i === '4') {
+                    res.json({subjects: subjects});
+                }
+            }
+        }
+    });
+});
+
+// return stories from NYT and Guardian matching subject provided in request body
+app.post('/topSeriousStories', function(req, res) {
+    var sqlResults = 'SELECT * FROM TopStories WHERE Subjects LIKE \'%' + req.body.subject  + '%\'; AND Source in (\'NYT\',\'Guardian\')';
+    conn.query(sqlResults, function(err, rows, fields) {
+        var stories = [];
+        if (err) {
+            console.log(err);
+        } else {
+            for (var i = 0; i < rows.length; i++) {
+                stories.push(rows[i]);
+                if (i === rows.length - 1) {
+                    res.json({results: stories});
+                }
+            }
+        }
+    });
+});
+
+// return top 5 tweet subjects from unfiltered tweet stream
 app.get('/tweetSubjects', function(req, res) {
     var subjects = {};
     var topSubjects = [];
@@ -108,6 +151,7 @@ app.get('/tweetSubjects', function(req, res) {
     });
 })
 
+// return up to 50 sample tweets from unfiltered tweet stream matching subject provided in request body
 app.post('/topTweets', function(req, res) {
     var sqlResults = 'SELECT * FROM Tweets WHERE Hashtag LIKE \'%' + req.body.subject  + '%\' ORDER BY Timestamp DESC LIMIT 50';
     connTweets.query(sqlResults, function(err, rows, fields) {
@@ -125,69 +169,60 @@ app.post('/topTweets', function(req, res) {
     });
 });
 
-/*var getTopSubjects = function() {
-    var subjects = [];
-    conn.query('SELECT * FROM TopSubjects', function(err, rows, fields) {
-        // Catch error in querying subjects
-        if(err) {
-            console.log(err);
-        } else {
-            console.log("Top News Subjects");
-            console.log("-----------------");
-            for (var i in rows) {
-                console.log(rows[i].Subject);
-                subject
-                subcount++;
-                if (subcount === 5) {
-                    logTwitterTrends();
-                }
-            }
-        }
-    });
-}
-
-var logTwitterTrends = function () {
-    var connTweets = mysql.createConnection({
-        host     : 'projectdb.cgoq09tt4tlc.us-west-2.rds.amazonaws.com',
-        port     : '3306',
-        user     : 'venciya',
-        password : 'venciyam',
-        database : 'TweetMap',
-    });
-
+// return top 5 tweet subjects from news filtered tweet stream
+app.get('/newsTweetSubjects', function(req, res) {
     var subjects = {};
+    var topSubjects = [];
 
-    connTweets.query('SELECT * FROM Tweets ORDER BY Timestamp DESC LIMIT 20000', function(err, rows, fields) {
+    connNewsTweets.query('SELECT * FROM Tweets ORDER BY Timestamp DESC LIMIT 20000', function(err, rows, fields) {
+      if (err) {
+        console.log(err);
+      } else {
+        for (var i in rows) {
+          var tagField = rows[i].Hashtag;
+          var tags = tagField.split(' ');
+          tags.pop();
+          for (var tag in tags) {
+            //console.log(tags[tag]);
+            var subject = tags[tag];
+            // discard foreign language characters
+            if (subject.search(/\?\?+/) === -1)
+            {
+              if (subjects[subject] === undefined && subject !== '') {
+                subjects[subject] = 1;
+              } else if (subject !== '') {
+                subjects[subject]++;
+              }
+            }
+          }
+        }
+        //console.log(subjects);
+        var keysSorted = Object.keys(subjects).sort(function(a,b) {return -(subjects[a] - subjects[b])});
+        //console.log(keysSorted);
+        for (var i = 0; i < 5 && i < keysSorted.length; i++) {
+          topSubjects.push(keysSorted[i]);
+          if (i === 4) {
+              res.json({subjects: topSubjects});
+          }
+        }
+      }
+    });
+})
+
+// return up to 50 sample tweets from news filtered tweet stream matching subject provided in request body
+app.post('/topNewsTweets', function(req, res) {
+    var sqlResults = 'SELECT * FROM Tweets WHERE Hashtag LIKE \'%' + req.body.subject  + '%\' ORDER BY Timestamp DESC LIMIT 50';
+    connNewsTweets.query(sqlResults, function(err, rows, fields) {
+        var tweets = [];
         if (err) {
             console.log(err);
         } else {
-            console.log('\n\nTop Twitter Hashtags');
-            console.log('-----------------');
-            for (var i in rows) {
-                var tagField = rows[i].Hashtag;
-                var tags = tagField.split(' ');
-                tags.pop();
-                for (var tag in tags) {
-                    //console.log(tags[tag]);
-                    var subject = tags[tag];
-                    // discard foreign language characters
-                    if (subject.search(/\?\?+/) === -1)
-    {
-        if (subjects[subject] === undefined && subject !== '') {
-            subjects[subject] = 1;
-        } else if (subject !== '') {
-            subjects[subject]++;
-        }
-    }
+            for (var i = 0; i < rows.length; i++) {
+                tweets.push(rows[i]);
+                if (i === rows.length - 1) {
+                    res.json({results: tweets});
                 }
-            }
-            //console.log(subjects);
-            var keysSorted = Object.keys(subjects).sort(function(a,b) {return -(subjects[a] - subjects[b])});
-            //console.log(keysSorted);
-            for (var i = 0; i < 5 && i < keysSorted.length; i++) {
-                console.log(keysSorted[i]);
             }
         }
     });
-}
-*/
+});
