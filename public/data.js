76a6f005ad767a89e47665c2ff8dcb1e6caad3f0
desrepair <kl2679@columbia.edu>
2015-12-22 02:52:44 +0000

    Working.

--- public/data.js
+++ public/data.js
@@ -127,6 +127,72 @@ function getTopNewsTweets(subject) {
 $(document).ready(function() {
     console.log('I am ready!');
     populateAllSubjects();
-    getTopStories('Cancer');
-    getTopSeriousStories('President of the United States');
+    $('#allnewsb1').click(function() {
+        getTopStories($('#allnewsb1')[0].textContent);
+    });
+    $('#allnewsb2').click(function() {
+        console.log($('#allnewsb2')[0].textContent);
+        getTopStories($('#allnewsb2')[0].textContent);
+    });
+    $('#allnewsb3').click(function() {
+        getTopStories($('#allnewsb3')[0].textContent);
+    });
+    $('#allnewsb4').click(function() {
+        getTopStories($('#allnewsb4')[0].textContent);
+    });
+    $('#allnewsb5').click(function() {
+        getTopStories($('#allnewsb5')[0].textContent);
+    });
+
+    $('#topnewsb1').click(function() {
+        getTopSeriousStories($('#topnewsb1')[0].textContent);
+    });
+    $('#topnewsb2').click(function() {
+        getTopSeriousStories($('#topnewsb2')[0].textContent);
+    });
+    $('#topnewsb3').click(function() {
+        getTopSeriousStories($('#topnewsb3')[0].textContent);
+    });
+    $('#topnewsb4').click(function() {
+        getTopSeriousStories($('#topnewsb4')[0].textContent);
+    });
+    $('#topnewsb5').click(function() {
+        getTopSeriousStories($('#topnewsb5')[0].textContent);
+    });
+
+    $('#twitterb1').click(function() {
+        getTopTweets($('#twitterb1')[0].textContent);
+    });
+    $('#twitterb2').click(function() {
+        getTopTweets($('#twitterb2')[0].textContent);
+    });
+    $('#twitterb3').click(function() {
+        getTopTweets($('#twitterb3')[0].textContent);
+    });
+    $('#twitterb4').click(function() {
+        getTopTweets($('#twitterb4')[0].textContent);
+    });
+    $('#twitterb5').click(function() {
+        getTopTweets($('#twitterb5')[0].textContent);
+    });
+    
+    $('#toptwitterb1').click(function() {
+        getTopNewsTweets($('#toptwitterb1')[0].textContent);
+    });
+    $('#toptwitterb2').click(function() {
+        getTopNewsTweets($('#toptwitterb2')[0].textContent);
+    });
+    $('#toptwitterb3').click(function() {
+        getTopNewsTweets($('#toptwitterb3')[0].textContent);
+    });
+    $('#toptwitterb4').click(function() {
+        getTopNewsTweets($('#toptwitterb4')[0].textContent);
+    });
+    $('#toptwitterb5').click(function() {
+        getTopNewsTweets($('#toptwitterb5')[0].textContent);
+    });
 });
+
+
+
+
